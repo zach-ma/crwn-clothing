40b4bfad52603264ffea3f50e76aaae01adda359
MJX-Git <63674024+MJX-Git@users.noreply.github.com>
2021-06-20 19:27:24 -0400

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
-import Homepage from "./pages/homepage/homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 const HatsPage = () => (
   <div>
@@ -15,7 +16,8 @@ function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={Homepage} />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
         <Route exact path="/shop/hats" component={HatsPage} />
       </Switch>
     </div>
